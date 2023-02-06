d0d9cac1b36559d5e4b6dac8ff5d9c27f7ef4054
Arpana <arp2ctaula@gmail.com>
2023-02-06 14:32:02 +0545

    Mouse Handle

---
+++ Unit3/MouseEventHandleDemo.java
@@ -0,0 +1,40 @@
+package Unit3;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.*;
+
+public class MouseEventHandleDemo {
+    public static void main(String[] args) {
+        JTextArea textarea = new JTextArea();
+        JFrame jf = new JFrame();
+        jf.add(new JScrollPane(textarea));
+        jf.setSize(400,400);
+        jf.setVisible(true);
+        jf.setLayout(null);
+
+        textarea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textarea.setText("Mouse Clicked: " + e.getX() + ", " + e.getY() + "\n");
+            }
+            @Override
+            public void mousePressed(MouseEvent e){
+                textarea.setText("Mouse Pressed: "+e.getX()+","+e.getY()+"\n");
+            }
+            @Override
+            public void  mouseReleased(MouseEvent e){
+                textarea.setText("Mouse Released: "+e.getX()+","+e.getY()+"\n");
+            }
+        });
+
+        textarea.addMouseMotionListener(new MouseMotionAdapter(){
+            @Override
+            public void mouseMoved(MouseEvent e){
+                textarea.setText("Mouse Moved: "+e.getX()+","+e.getY()+"\n");
+            }
+        });
+    }
+    
+}
