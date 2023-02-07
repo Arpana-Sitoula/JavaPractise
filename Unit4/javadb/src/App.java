88f64119a9ff560204fc69664d326af1db382acb
Arpana <arp2ctaula@gmail.com>
2023-02-07 20:06:20 +0545

    Unit4 added

---
+++ Unit4/javadb/src/App.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            // Load the JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // Connect to the database
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root","");
+
+            // Execute a query to retrieve data from the database
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+            // Loop through the result set and print the data
+            while (rs.next()) {
+                System.out.println("name: " + rs.getString("name"));
+                System.out.println("address: " + rs.getString("address"));
+            }
+
+            // Close the connection
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
