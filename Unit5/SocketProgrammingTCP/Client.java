6374182f35e96c809935e06fbf08445de3011d76
Arpana <arp2ctaula@gmail.com>
2023-02-08 21:40:29 +0545

    Organized

--- Unit5/SocketProgrammingTCP/Client.java
+++ Unit5/SocketProgrammingTCP/Client.java
@@ -5,11 +5,20 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws Exception{
+        //Step1: create a socket object
         Socket s = new Socket("localhost",2345);
+
+        //Step2: create an output stream that can be used to send info. to the Socket
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+       //Step3: create an input stream to read the response from server (wo don't do that here)
+      
+       //Step4: do I/O with input and output streams
         dout.writeUTF("Helloooooo");
         dout.flush();
         dout.close();
+        
+         //Step5: close the socket when done
         s.close();
     }
 }
--- Unit5/SocketProgrammingTCP/Server.java
+++ Unit5/SocketProgrammingTCP/Server.java
@@ -6,12 +6,22 @@ import java.net.Socket;
 
 public class Server{
     public static void main(String[] args) throws Exception{
+
+        //step1: create socketport for incoming request
         ServerSocket ss = new ServerSocket(2345);
-        //establish connection
+
+        //step2: wait for incoming connection request
         Socket s = ss.accept();
+
+        //step3: read request from connection socket
         DataInputStream dis = new DataInputStream(s.getInputStream());
+
+        //step4: write reply to connection socket (we don't do that here)
+        
         String str = dis.readUTF();
         System.out.println("Client message = "+ str);
+
+        //step5: close connection socket
         ss.close();
 }
 }
