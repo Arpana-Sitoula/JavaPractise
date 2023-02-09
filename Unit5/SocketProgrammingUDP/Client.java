a74640e8ca3bd703ce6b24f465cbf72fb7d6ed94
Arpana <arp2ctaula@gmail.com>
2023-02-09 08:20:15 +0545

    UDP organized

--- Unit5/SocketProgrammingUDP/Client.java
+++ Unit5/SocketProgrammingUDP/Client.java
@@ -6,12 +6,18 @@ import java.net.InetAddress;
 
 public class Client {
     public static void main(String[] args) throws Exception{
+
+        //get a datagram socket
         DatagramSocket socket = new DatagramSocket();
+
+        //send request
         InetAddress add = InetAddress.getLocalHost();
         String msg = "hellooooo";
         byte[] data = msg.getBytes();
         DatagramPacket packet = new DatagramPacket(data, data.length,add, 2345);
         socket.send(packet);
+
+        //close socket
         socket.close();
     }
 }
--- Unit5/SocketProgrammingUDP/Server.java
+++ Unit5/SocketProgrammingUDP/Server.java
@@ -6,15 +6,22 @@ import java.net.InetAddress;
 
 public class Server {
     public static void main(String[] args) throws Exception{
+
+        //get a datagram socket
         DatagramSocket socket = new DatagramSocket(2345);
+        
         InetAddress add = InetAddress.getLocalHost();
 
         byte myBuffer[] = new byte[2000];
         DatagramPacket dp = new DatagramPacket(myBuffer,myBuffer.length, add,2345);
+        
+        //receive request
         socket.receive(dp);
         System.out.println("Message Received: ");
         String rec = new String(dp.getData());
         System.out.println(rec);
+
+        //close socket
         socket.close();
     }
 }
