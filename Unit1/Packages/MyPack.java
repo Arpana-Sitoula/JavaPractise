406f5fd3fa1479337188641ccd89cb73d4824f6e
Arpana <arp2ctaula@gmail.com>
2023-03-23 11:29:53 +0545

    package of Unit1

---
+++ Unit1/Packages/MyPack.java
@@ -0,0 +1,8 @@
+package Unit1.Packages;
+
+public class MyPack {
+        public void msg(){
+        System.out.println("welcome to my package");
+        }
+    }
+
---
+++ Unit1/Packages/YourPack.java
@@ -0,0 +1,9 @@
+package Unit1.Packages;
+
+
+public class YourPack {
+    public static void main(String[] args) {
+        MyPack obj = new MyPack();
+        obj.msg();
+    }
+}
