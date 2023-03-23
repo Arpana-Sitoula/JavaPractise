f8d2079c83fddaffd68a4d44fedd6d51e4b5a1be
Arpana <arp2ctaula@gmail.com>
2023-03-23 13:12:15 +0545

    Rectangle calc using Oop

---
+++ Unit1/Exercises/Rectangle.java
@@ -0,0 +1,24 @@
+package Unit1.Exercises;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(5.0, 3.0);
+        System.out.println("Area: " + rect.getArea());
+        System.out.println("Perimeter: " + rect.getPerimeter());
+    }
+}
